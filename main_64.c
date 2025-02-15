db82ff39f26b2e5d234caff35cf28cf9ae94b3a9
D0Dan <chekanov.dan@gmail.com>
2025-02-15 17:14:49 -0600

    added tiemr (again)

--- main_64.c
+++ main_64.c
@@ -2,31 +2,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 int main(int argc, char* argv[]) 
 {
 	size_t bytes_read; 
 	char buffer[64];  // 64 byte buffer
-	int input_file = open("input.txt", O_RDONLY); // input file-strean for read()
+	int input_file = open("input.txt", O_RDONLY); // input file-stream
 
-	// preform read on input_file until '.' is encountared or no bytes are read
+	clock_t start, end;
+	double cpu_time;
+
+	start = clock(); // start of timer 
+
+	// preform read on input_file until '.' is encountered or no bytes are read
 	for(int index = 0; (bytes_read = read(input_file, buffer, 64)) != 0; index++)
 	{
 		for (int i = 0; i <= bytes_read; i++)
 		{	
-			printf("-> %c at %i\n", buffer[i], i);
+			//printf("-> %c at %i\n", buffer[i], i);
 			if (buffer[i] == '.') 
 			{
 				printf("period detected at position: %i \n", index*64 + i);
+				end = clock(); // end of timer
+				printf("time taken: %f miliseconds\n", ((double) (end - start)) / CLOCKS_PER_SEC * 1000.0);
 				close(input_file);
 				return 0;
 			}
 		}
 	} 
 
+	end = clock(); // end of timer if error/no period
+
 	if(bytes_read == -1) 
 	{
 		printf("error\n");
+		printf("time taken: %f miliseconds\n", ((double) (end - start)) / CLOCKS_PER_SEC * 1000.0);
 		close(input_file);
 		exit(1);	       
 	} 
@@ -34,6 +45,7 @@ int main(int argc, char* argv[])
 	{
 		printf("bytes read : %i\n", bytes_read);
 		printf("no period detected\n");
+		printf("time taken: %f miliseconds\n", ((double) (end - start)) / CLOCKS_PER_SEC * 1000.0);
 	}
 
 	close(input_file);
